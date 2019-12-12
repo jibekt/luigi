021865156e90623dde0a012e9605d9341fa3afff
zhibek <zhibekt@gmail.com>
2019-12-12 22:00:04 +0100

    Controller commit

---
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -0,0 +1,26 @@
+package be.vdab.luigi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping
+public class IndexController {
+    @GetMapping
+    public String index() {
+        StringBuffer buffer =
+                new StringBuffer("<!doctype html><html><title>Hallo</title><body>");
+        int uur = LocalTime.now().getHour();
+        if (uur < 12) {
+            buffer.append("Goede morgen");
+        } else if (uur < 18) {
+            buffer.append("Goede middag");
+        } else {
+            buffer.append("Goede avond");
+        }
+        buffer.append("</body></html>");
+        return buffer.toString();
+    }
+}
