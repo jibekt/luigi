dc45a5142a41905988906b8dda2c3b4ab377cb65
zhibek <zhibekt@gmail.com>
2019-12-13 00:37:16 +0100

    Adres en Persoon added

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,20 +1,40 @@
 package be.vdab.luigi.controllers;
 
+import be.vdab.luigi.domain.Adres;
+import be.vdab.luigi.domain.Persoon;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 //@RestController
 @Controller
 @RequestMapping
 public class IndexController {
+
+    private String boodschap() {
+        int uur = LocalTime.now().getHour();
+        if (uur < 12) {
+            return "morgen";
+        }
+        if (uur < 18) {
+            return "middag";
+        }
+        return "avond";
+    }
    @GetMapping
     public ModelAndView index(){
-       int uur = LocalTime.now().getHour();
+        ModelAndView modelAndView = new ModelAndView("index", "boodschap", boodschap());
+        modelAndView.addObject("zaakvoerder", new Persoon("Luigi", "Peperone",7, true, LocalDate.of(1966,1,31),new Adres("Grote Markt", "3", 9700, "Oudenaarde")));
+        return modelAndView;
+
+
+   }
+       /*int uur = LocalTime.now().getHour();
        if (uur <12){
            return new ModelAndView("index", "boodschap", "morgen");
        }
@@ -46,6 +66,6 @@ public class IndexController {
 
 
 
-    }
+
 
 }
---
+++ src/main/java/be/vdab/luigi/domain/Adres.java
@@ -0,0 +1,31 @@
+package be.vdab.luigi.domain;
+
+public class Adres {
+    private String straat;
+    private String huisNr;
+    private int postcode;
+    private String gemeente;
+
+    public String getStraat() {
+        return straat;
+    }
+
+    public String getHuisNr() {
+        return huisNr;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public String getGemeente() {
+        return gemeente;
+    }
+
+    public Adres(String straat, String huisNr, int postcode, String gemeente) {
+        this.straat = straat;
+        this.huisNr = huisNr;
+        this.postcode = postcode;
+        this.gemeente = gemeente;
+    }
+}
