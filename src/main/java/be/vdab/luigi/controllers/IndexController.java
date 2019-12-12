14bd6aa57105c60dfffc940f1f107819713f4f1a
zhibek <zhibekt@gmail.com>
2019-12-12 23:41:55 +0100

    Thymeleaf commit

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,16 +1,33 @@
 package be.vdab.luigi.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.time.LocalTime;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping
 public class IndexController {
-    @GetMapping
-    public String index() {
-        StringBuffer buffer =
+   @GetMapping
+    public ModelAndView index(){
+       int uur = LocalTime.now().getHour();
+       if (uur <12){
+           return new ModelAndView("index", "boodschap", "morgen");
+       }
+       if(uur < 18){
+           return new ModelAndView("index", "boodschap", "middag");
+       }
+       return new ModelAndView("index", "boodschap", "avond");
+
+
+
+    /*public String index() {
+         ====================
+       StringBuffer buffer =
                 new StringBuffer("<!doctype html><html><title>Hallo</title><body>");
         int uur = LocalTime.now().getHour();
         if (uur < 12) {
@@ -22,5 +39,13 @@ public class IndexController {
         }
         buffer.append("</body></html>");
         return buffer.toString();
+        =======================
+       return "index";
+        =======================  */
+
+
+
+
     }
+
 }
