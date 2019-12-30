d48c930c1e3314fecb7b6b71584f877aea0d2fec
zhibek <zhibekt@gmail.com>
2019-12-30 01:04:48 +0100

    bean validation

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 
 
@@ -75,7 +76,7 @@ public class PizzaController {
                 .addObject(new VanTotPrijsForm(null,null));
     }
     @GetMapping("vantotprijs")
-    public ModelAndView vanTotPrijs(VanTotPrijsForm form, Errors errors) {
+    public ModelAndView vanTotPrijs(@Valid VanTotPrijsForm form, Errors errors) {
         ModelAndView modelAndView = new ModelAndView("vantotprijs");
         if (errors.hasErrors()) {
             return modelAndView;
